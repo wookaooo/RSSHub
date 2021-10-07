0e656f9e7c19c149b4b5d9cfa7e508954941194b
wookaooo <68209998+wookaooo@users.noreply.github.com>
2021-10-08 01:20:51 +0800

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+const isProd = process.env.NODE_ENV === "production";
+module.exports = {
+  env: {
+    STATIC_URL: isProd ? process.env.STATIC_URL : "",
+  },
+  assetPrefix: isProd ? process.env.STATIC_URL : "",
+};
